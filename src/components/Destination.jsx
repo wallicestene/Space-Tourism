73b2dadf8868ee365fcca34b050f2e814a0fa0e1
Your Name <wallicestenewaweru@gmail.com>
2023-06-07 22:31:56 +0300

    moon

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -4,7 +4,7 @@ import data from '../data.json';
 function Destination() {
 
   const [value, setvalue] = useState(0)
-  const destinationName = ["Moon","Titan","Mars", "Europa"]; // Replace with your logic
+  const destinationName = ["Moon","Mars","Titan","Europa"]; // Replace with your logic
 
   // Findind the selected destination from the data
   const selectedDestination = data.destinations.find(
@@ -15,9 +15,9 @@ function Destination() {
     <div className='md:bg-[url(/assets/destination/background-destination-desktop.jpg)] bg-[url(/assets/destination/background-destination-tablet.jpg)] bg-cover h-screen text-white bg-no-repeat overflow-scroll relative'>
       <div className='mt-5'>
         <Navbar/>
-        <h1 className='text-center md:text-left md:ml-20 font-barlow text-xl tracking-widest uppercase'><span className=' text-gray-400'>01 </span>Pick Your Destination</h1>
+        <h1 className='text-center md:text-left md:ml-20 font-barlow text-xl tracking-widest uppercase mb-5'><span className=' text-gray-400'>01 </span>Pick Your Destination</h1>
       </div>
-      <div className='grid md:grid-cols-2 grid-rows-1 gap-7  mx-10 place-items-center h-screen'>
+      <div className='grid md:grid-cols-2 grid-rows-1 gap-7  mx-10 md:place-items-center md:h-screen'>
       <div className='left grid place-items-center'>
         {/* Display the selected destination image */}
         <img src={selectedDestination.images.webp} alt={selectedDestination.name} className=' md:w-4/5 object-contain w-4/5 animate-spin-slow'/>
@@ -28,13 +28,13 @@ function Destination() {
         {
           destinationName.map((destination, index) => (
           <p 
-          className={`${index === value && "border-b-2 opacity-70 "} pb-1 hover:opacity-60 hover:cursor-pointer mb-2 text-base uppercase tracking-wider hover:border-b-2`}
+          className={`${index === value && "border-b-2 opacity-70 "} pb-1 hover:opacity-60 hover:cursor-pointer text-base mb-5 uppercase tracking-wider hover:border-b-2`}
           key={index} 
           onClick={() =>setvalue(index)}>{destination}</p>
           ))}
         </div>
         <h2 className='text-8xl mb-5 font-bellefair font-semibold'>{selectedDestination.name}</h2>
-        <p className=' text-base mb-10'>{selectedDestination.description}</p>
+        <p className=' text-base md:10 mb-5'>{selectedDestination.description}</p>
         <hr />
         <div className='flex items-center md:justify-between px-5 justify-between flex-wrap gap-6'> 
           <div >
